2143ea3a6d7b01f1c32251f7203f6c3694d562b5
yadhumanikandan <yadhumanikandan0@gmail.com>
2023-10-03 17:38:01 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import subprocess
+
+
+def cmd():
+    # Your terminal command as a string
+    command = "dmesg | grep -i hid"
+
+    # Run the command and capture the result
+    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+    # Check if the command was successful
+    if result.returncode == 0:
+        # Command was successful, capture the output
+        output = result.stdout
+        print("Command Output:")
+        print(output)
+    else:
+        # Command failed, capture the error message
+        error_message = result.stderr
+        print("Command Error:")
+        print(error_message)
+
+
+cmd()
