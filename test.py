7e0b9cdbf727440b873cabd430342ffb0f3b57f8
yadhumanikandan <yadhumanikandan0@gmail.com>
2023-10-07 21:38:10 +0530

    test

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 import usb.core
 
-busses = usb.core.busses()
-for bus in busses:
-    devices = bus.core.devices
-    for dev in devices:
-        #if "respberry" in dev.filename.lower():
-        print(f"Device: {dev.filename}")
-        print(f"idVendor: {dev.idVendor} (0x{dev.idVendor:x})")
-        print(f"idProduct: {dev.idProduct} (0x{dev.idProduct:x})")
+# Find USB devices
+devices = usb.core.find(find_all=True)
+
+# Print information about each device
+for device in devices:
+    print("Vendor ID: 0x{:04x}".format(device.idVendor))
+    print("Product ID: 0x{:04x}".format(device.idProduct))
+    print("Manufacturer:", usb.util.get_string(device, device.iManufacturer))
+    print("Product:", usb.util.get_string(device, device.iProduct))
+    print()
