2286700d3b364b00df4117d939fd299c7cd139cb
yadhumanikandan <yadhumanikandan0@gmail.com>
2023-10-13 00:25:39 +0530

    test

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import subprocess
 import re
 import json
 
-output = ""
 
 def search_usb():
     # Your terminal command as a string
@@ -13,27 +12,16 @@ def search_usb():
 
     # Check if the command was successful
     if result.returncode == 0:
-        # Command was successful, capture the output
         output = result.stdout
-        #print("Command Output:")
-        #print(output)
         process(output)
 
     else:
-        # Command failed, capture the error message
         error_message = result.stderr
-        #print("Command Error:")
         print(error_message)
-
-
-# def checkExist():                       ## check if the log timestamp already exists
-
 
 
 
 def process(out):                       ## process the command output and timestamp
-    # print(out)
-    # timestamp_pattern = r'\[(\s*\d+\.\d+)\]'
 
     output_lines =out.strip().split('\n')
 
@@ -60,7 +48,7 @@ def process(out):                       ## process the command output and timest
         
         for line in difference:
             log_data.append({"event": line})
-        # existing_data.append(difference)
+
 
         with open('log.json', 'w') as json_file:
             json.dump(log_data, json_file, indent=4)
@@ -73,28 +61,6 @@ def process(out):                       ## process the command output and timest
             json.dump(log_data, json_file, indent=4)
 
 
-
-    # with open('log.json', 'w') as json_file:
-    #     json.dump(log_data, json_file, indent=4)
-
-    # timestamps = [re.search(timestamp_pattern, line).group(1) for line in output_lines]
-    
-    # for lines in output_lines:
-        # print(lines + "\n\n")
-        
-
-    # print(timestamps)
-
-
-
-# def write(log):
-#     file_path = "log.txt"
-
-#     with open(file_path, "a") as file:
-#         file.write("\n\n" + log)
-
-# while True:
-#     search_usb()
 
 
 search_usb()
