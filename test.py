046f6fc7b7c59cfccf519fc0390cf68f5e515516
yadhumanikandan <yadhumanikandan0@gmail.com>
2023-10-13 20:13:29 +0530

    test

--- test.py
+++ test.py
@@ -1,10 +1,20 @@
-import notify2
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
 
-# Initialize the notify2 library
-notify2.init("Your App Name")
+class MyWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Window with Close Button")
+        self.connect("destroy", Gtk.main_quit)
 
-# Create a notification
-notification = notify2.Notification("Title of the Notification", "Message of the Notification")
+        self.button = Gtk.Button(label="Close")
+        self.button.connect("clicked", self.on_close_button_clicked)
 
-# Show the notification
-notification.show()
+        self.add(self.button)
+
+    def on_close_button_clicked(self, widget):
+        Gtk.main_quit()
+
+win = MyWindow()
+win.show_all()
+Gtk.main()
