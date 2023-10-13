93576e09a5183b55e8d81bbccb6be48dec786f0b
yadhumanikandan <yadhumanikandan0@gmail.com>
2023-10-13 23:44:50 +0530

    test

--- tktest.py
+++ tktest.py
@@ -1,21 +1,24 @@
 import tkinter as tk
+import subprocess
 
 # Function to close the message window
 def close_message_window():
-    message_window.destroy()
+    command = "python3 /home/manik/project/cp/tktest.py"
+    root.destroy()
+    subprocess.run(command, shell=True)
 
 # Create the main window
 root = tk.Tk()
 root.title("Message Window Example")
-root.geometry("400x200")  # Adjust the window size here (width x height)
+root.geometry("400x150")  # Adjust the window size here (width x height)
 
 # Create a label with the message
-message_label = tk.Label(root, text="This is a message.")
-message_label.pack(pady=30)  # Increase pady for more space
+message_label = tk.Label(root, text="This is a message.", fg="red", font=("Helvetica", 18))
+message_label.pack(pady=25)  # Increase pady for more space
 
 # Create a button at the bottom to close the window
-close_button = tk.Button(root, text="OK", command=close_message_window)
-close_button.pack(pady=20)  # Increase pady for more space
+close_button = tk.Button(root, text="OK", command=close_message_window, width=10, height=3)
+close_button.pack(pady=10)  # Increase pady for more space
 
 # Run the tkinter main loop
 root.mainloop()
