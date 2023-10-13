db0bf9b5544d163e6aabd5dcb0fb95c7b4aecdb3
yadhumanikandan <yadhumanikandan0@gmail.com>
2023-10-13 23:47:24 +0530

    test

--- tktest.py
+++ tktest.py
@@ -9,15 +9,15 @@ def close_message_window():
 
 # Create the main window
 root = tk.Tk()
-root.title("Message Window Example")
+root.title("Device detected")
 root.geometry("400x150")  # Adjust the window size here (width x height)
 
 # Create a label with the message
-message_label = tk.Label(root, text="This is a message.", fg="red", font=("Helvetica", 18))
+message_label = tk.Label(root, text="A suspected device has been connected", fg="red", font=("Helvetica", 18))
 message_label.pack(pady=25)  # Increase pady for more space
 
 # Create a button at the bottom to close the window
-close_button = tk.Button(root, text="OK", command=close_message_window, width=10, height=3)
+close_button = tk.Button(root, text="Show Log", command=close_message_window, width=10, height=3)
 close_button.pack(pady=10)  # Increase pady for more space
 
 # Run the tkinter main loop
