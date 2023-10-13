b17c7b67284cbbcb8398167a008ab411f701f664
yadhumanikandan <yadhumanikandan0@gmail.com>
2023-10-13 23:23:27 +0530

    test

--- tktest.py
+++ tktest.py
@@ -7,14 +7,15 @@ def close_message_window():
 # Create the main window
 root = tk.Tk()
 root.title("Message Window Example")
+root.geometry("400x200")  # Adjust the window size here (width x height)
 
 # Create a label with the message
 message_label = tk.Label(root, text="This is a message.")
-message_label.pack(pady=20)
+message_label.pack(pady=30)  # Increase pady for more space
 
 # Create a button at the bottom to close the window
 close_button = tk.Button(root, text="OK", command=close_message_window)
-close_button.pack(pady=10)
+close_button.pack(pady=20)  # Increase pady for more space
 
 # Run the tkinter main loop
 root.mainloop()
