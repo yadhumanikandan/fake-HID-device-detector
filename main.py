128f45fe4a63ef3adaa0897004e0df35e16d0bf6
yadhumanikandan <yadhumanikandan0@gmail.com>
2023-10-13 19:06:38 +0530

    test

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 # fake HID device detector
 
 import subprocess
-import re
 import json
+import datetime
 
 
 def search_usb():
-    # Your terminal command as a string
     command = "dmesg | grep -i hid-generic"
 
-    # Run the command and capture the result
+    current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
-    # Check if the command was successful
     if result.returncode == 0:
         output = result.stdout
-        process(output)
+        process(output, current_time)
 
     else:
         error_message = result.stderr
@@ -23,41 +21,52 @@ def search_usb():
 
 
 
-def process(out):                       ## process the command output and timestamp
+def process(output, time):                       ## process the command output
 
-    output_lines =out.strip().split('\n')
+    output_lines =output.strip().split('\n')
 
     log_data = []
 
-    existing_data = []
+    existing_events = []
 
     with open('log.json', 'r') as json_file:
         log = json.load(json_file)
 
     # Check if the file has data
     if log:
-        for entry in log:
-            event = entry.get("event")
-            if event is not None:
-                existing_data.append(event)                     # read all existing log to program
+        # for entry in log:
+        #     event = entry.get("event")
+        #     if event is not None:
+        #         existing_data.append(event)                     # read all existing log to program
         
-        difference = list(set(output_lines) - set(existing_data))       #find its difference
+        existing_events = [d["event"] for d in log]
 
-        log_data = []
+        difference = [event for event in output_lines if event not in existing_events]
 
-        for line in existing_data:
-            log_data.append({"event": line})
+        # difference = list(set(output_lines) - set(existing_events))       #find its difference
+
+        # log_data = []
+
+        # for line in existing_events:
+        #     log_data.append({"event": line})
         
-        for line in difference:
-            log_data.append({"event": line})
+        # for line in difference:
+        #     log_data.append({"event": line})
+
+        # current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        # Append events to list_of_dicts with the current time
+        for event in difference:
+            new_dict = {"event": event, "time": time}
+            log.append(new_dict)
 
 
         with open('log.json', 'w') as json_file:
-            json.dump(log_data, json_file, indent=4)
+            json.dump(log, json_file, indent=4)
 
     else:
         for line in output_lines:
-            log_data.append({"event": line})
+            log_data.append({"event": line, "time": time})
 
         with open('log.json', 'w') as json_file:
             json.dump(log_data, json_file, indent=4)
