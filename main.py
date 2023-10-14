d9413b13c173a157c687de90402ca8deea04e80b
yadhumanikandan <yadhumanikandan0@gmail.com>
2023-10-14 14:17:30 +0530

    test

--- main.py
+++ main.py
@@ -69,9 +69,8 @@ def save(output, time):
         for event in difference:
             data = extract(event)
             log.append({"event": event, "time": time, "name": data["name"], "suspected": data["suspected"], "vid": data["vid"], "pid": data["pid"]})
-            # if data["suspected"] == "true":
-            #     popup()
-            print(data["suspected"])
+            if data["suspected"] == "True":
+                popup()
 
         with open('log.json', 'w') as json_file:
             json.dump(log, json_file, indent=4)
