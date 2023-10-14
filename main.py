b01bae1c5a506bf4cfb8c4e7b9fb668afdb99c39
yadhumanikandan <yadhumanikandan0@gmail.com>
2023-10-14 12:45:00 +0530

    test

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import subprocess
 import json
 import datetime
+import re
 
 
 def search_usb():
@@ -20,18 +21,27 @@ def search_usb():
         print(error_message)
 
 
-def checkEvent(event):
+def extract(event):
     suspected = ["Raspberry Pi"]
     check = 0
+
+    pattern = r"\[.*\] hid-generic (\w+:\w+:\w+\.\w+):.*\[(.*?)\] on .*"
+
+    match = re.search(pattern, event)
+
+    if match:
+        device_id = match.group(1)
+        device_name = match.group(2)
+        vendor_id, product_id, _ = device_id.split(':')
 
     for s in suspected:
         if s in event:
             check = 1
     
     if check == 0:
-        return False
+        return {"suspected": False, "name": device_name, "vid": vendor_id, "pid": product_id}
     else:
-        return True
+        return {"suspected": True, "name": device_name, "vid": vendor_id, "pid": product_id}
 
 
 def save(output, time):
@@ -52,8 +62,8 @@ def save(output, time):
         difference = [event for event in output_lines if event not in existing_events]
 
         for event in difference:
-            new_dict = {"event": event, "time": time, "suspected": checkEvent(event)}
-            log.append(new_dict)
+            data = extract(event)
+            log.append({"event": event, "time": time, "name": data["name"], "suspected": data["suspected"], "vid": data["vid"], "pid": data["pid"]})
 
 
         with open('log.json', 'w') as json_file:
@@ -61,12 +71,18 @@ def save(output, time):
 
     else:
         for event in output_lines:
-            log_data.append({"event": event, "time": time, "suspected": checkEvent(event)})
+            data = extract(event)
+            log_data.append({"event": event, "time": time, "name": data["name"], "suspected": data["suspected"], "vid": data["vid"], "pid": data["pid"]})
 
         with open('log.json', 'w') as json_file:
             json.dump(log_data, json_file, indent=4)
 
 
 
-while True:
-    search_usb()
+def main():
+    while True:
+        search_usb()
+
+
+if __name__ == "__main__":
+    main()
