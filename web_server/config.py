9a4b88c202e63cd9eee0c6a5f0e45c22264ed2e9
yadhumanikandan <yadhumanikandan0@gmail.com>
2025-02-19 22:45:23 +0530

    mail error fix

--- web_server/config.py
+++ web_server/config.py
@@ -10,6 +10,7 @@ app.secret_key = "lasdfoh389h9qhweohpqe8hgqh9hqwh49hq9hgq9h"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable modification tracking
 db = SQLAlchemy(app)
+app.debug = True
 
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_PERMANENT'] = True
